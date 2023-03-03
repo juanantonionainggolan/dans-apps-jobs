21e0735504a653f35102cb155cba5df299de358f
Juan Antonio Nainggolan <juanantonionainggolan@Juans-MacBook-Air.local>
2023-03-04 02:56:50 +0700

    Create dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/dashboard" element={<><Navbar/><Dashboard/></>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,19 +8,19 @@ const Login = () => {
         <div className="columns is-centered">
             <div className='column is-4-desktop'>
                 <form className='box'>
-                    <div className='field'>
+                    <div className='field mt-5'>
                         <label className='label'>Email or Username</label>
                         <div className='controls'>
                             <input type="text" className='input' placeholder='username' />
                         </div>
                     </div>
-                    <div className='field'>
+                    <div className='field mt-5'>
                         <label className='label'>Password</label>
                         <div className='controls'>
                             <input type="password" className='input' placeholder='******' />
                         </div>
                     </div>
-                    <div className='field'>
+                    <div className='field mt-5'>
                         <button className='button is-success is-fullwidth'>Login</button>
                     </div>
                 </form>
